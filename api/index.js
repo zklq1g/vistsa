f9847f78e5510674662ea7231874aee87d379266
Izuku <zklq1e@gmail.com>
2026-03-08 21:02:04 +0530

    fix: correct broken require path in api/index.js (./server -> ../server) - root cause of all 500 errors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./server/src/app');
+const app = require('../server/src/app');
 
 // Vercel serverless functions require exporting the express app directly
 // rather than calling app.listen()
